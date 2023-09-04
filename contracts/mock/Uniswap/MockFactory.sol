a2d3b576b5f58c08f8fa2d0a257e948dce221e4e
Smea1 <1476977430@qq.com>
2023-09-04 17:02:57 +0800

    delete the useless code

--- contracts/mock/Uniswap/MockFactory.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity =0.5.16;
-import "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
-
-contract MockUniswapV2Factory is UniswapV2Factory {
-    constructor(address _feeToSetter) public UniswapV2Factory(_feeToSetter) {}
-}
--- contracts/mock/Uniswap/MockRouter.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.6.6;
-import "@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
-
-contract MockUniswapV2Router is UniswapV2Router02 {
-    constructor(
-        address _factory,
-        address _WETH
-    ) public UniswapV2Router02(_factory, _WETH) {}
-}
